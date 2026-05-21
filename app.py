b0fe9941c9c5d2a8f4536f3808a9658e9932702a
Kulsoom <kulsoomhameed47@gmail.com>
2026-05-21 13:00:17 +0500

    ML APP

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ area_sq=st.number_input("Enter an area in SQ-FT",min_value=500) #kam  se kam rkh
 if st.button("Predict Price"):
     input_data=[[area_sq]]
     predicted_price=area.predict(input_data)
-    st.success(f"Your Predicted Price For {area_sq} is: {predicted_price}") 
+    st.success(f"Your Predicted Price For {area_sq} is : ${int(r2)}") 
